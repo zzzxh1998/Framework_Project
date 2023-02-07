03209e20d8737b7c3c6e9f1886c02192cad6addc
zhangxiaohu <zhangxiaohu@nti56.com>
2023-02-07 17:16:03 +0800

    SpringBoot学习

---
+++ 备忘录/config/DruidStatFilter.java
@@ -0,0 +1,11 @@
+package com.xiaohu.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+@WebFilter(filterName = "druidWebStatFilter",urlPatterns = "/*",initParams = {@WebInitParam(name = "exclusions",value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")})
+public class DruidStatFilter extends WebStatFilter {
+
+}
